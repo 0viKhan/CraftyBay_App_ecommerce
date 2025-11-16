f946f1403f13969b6b4b4f0f51969d67523921d6
0viKhan <hossain15-5176@diu.edu.bd>
2025-11-16 11:58:06 +0600

    Initial commit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class CraftyBay extends StatefulWidget {
   const CraftyBay({super.key});
@@ -25,7 +26,19 @@ class _CraftyBayState extends State<CraftyBay> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+
+
       navigatorObservers: [observer],
+      locale: Locale('en'),
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('bn'), // Spanish
+      ],
 
     );
   }
