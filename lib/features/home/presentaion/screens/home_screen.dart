aac1c02e81af09e71c3ffaba685f084c36a002db
0viKhan <hossain15-5176@diu.edu.bd>
2025-11-20 19:22:39 +0600

    SignIn Screen

--- lib/features/home/presentaion/screens/home_screen.dart
+++ lib/features/home/presentaion/screens/home_screen.dart
@@ -1,7 +1,13 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_commerce_shop/app/extensions/app_colors.dart';
+import 'package:e_commerce_shop/features/home/presentaion/screens/home_screen.dart';
+import 'package:e_commerce_shop/features/home/widgets/home_banner_slider.dart';
 import 'package:e_commerce_shop/features/shared/presentation/widgets/app_bar_icon_button.dart';
 import 'package:flutter/cupertino.dart'
     show Placeholder, StatefulWidget, State, BuildContext, Widget;
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -23,11 +29,9 @@ class _HomeScreenState extends State<HomeScreen> {
           height: 150,
         ),
         actions: [
-          AppBarIconButton(onTap: () {  }, iconData:Icons.person,),
-          AppBarIconButton(onTap: () {  }, iconData:Icons.call,),
-          AppBarIconButton(onTap: () {  }, iconData:Icons.notification_add,)
-
-
+          AppBarIconButton(onTap: () {}, iconData: Icons.person),
+          AppBarIconButton(onTap: () {}, iconData: Icons.call),
+          AppBarIconButton(onTap: () {}, iconData: Icons.notification_add),
         ],
       ),
       body: Padding(
@@ -35,33 +39,53 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             const SizedBox(height: 16),
-            _build_search_bar()
+            _build_search_bar(),
+            const SizedBox(height: 16),
+            HomeBannerSlider(),
+            buildSelectionHeader(title: 'Categories', onTapSeeAll: () {}),
+            const SizedBox(height: 16),
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: AppColors.themeColor.withOpacity(0.4)
+              ),
+            ),
+            buildSelectionHeader(title: 'New', onTapSeeAll: () {}),
+            const SizedBox(height: 16),
+
+            buildSelectionHeader(title: 'Popular', onTapSeeAll: () {}),
           ],
         ),
       ),
     );
   }
 
+  Widget buildSelectionHeader({
+    required String title,
+    required VoidCallback onTapSeeAll,
+  }) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(title, style: Theme.of(context).textTheme.titleMedium),
+        TextButton(onPressed: onTapSeeAll, child: Text("See all")),
+      ],
+    );
+  }
+
   Widget _build_search_bar() {
     return TextField(
-            onSubmitted: (String? text){
+      onSubmitted: (String? text) {},
+      textInputAction: TextInputAction.search,
+      decoration: InputDecoration(
+        hintText: 'Search',
 
-            },
-            textInputAction:TextInputAction.search,
-            decoration: InputDecoration(
-              hintText: 'Search',
-
-              fillColor: Colors.grey.shade300,
-              filled: true,
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide.none,
-
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide.none
-              ),
-              prefixIcon: Icon(Icons.search),
-            ),
-          );
+        fillColor: Colors.grey.shade300,
+        filled: true,
+        enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        prefixIcon: Icon(Icons.search),
+      ),
+    );
   }
 }
