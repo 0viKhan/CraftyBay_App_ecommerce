a7dbedb9ee56d0c0f29bce7b7ef0292192d33a5b
0viKhan <hossain15-5176@diu.edu.bd>
2025-11-24 20:01:59 +0600

    Category and Home

--- lib/features/home/presentaion/screens/home_screen.dart
+++ lib/features/home/presentaion/screens/home_screen.dart
@@ -1,7 +1,12 @@
+import 'package:e_commerce_shop/features/home/widgets/home_banner_slider.dart';
+import 'package:e_commerce_shop/features/shared/presentation/controller/main_nav_controller.dart';
 import 'package:e_commerce_shop/features/shared/presentation/widgets/app_bar_icon_button.dart';
+import 'package:e_commerce_shop/features/shared/presentation/widgets/product_category_item.dart';
 import 'package:flutter/cupertino.dart'
     show Placeholder, StatefulWidget, State, BuildContext, Widget;
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -23,11 +28,9 @@ class _HomeScreenState extends State<HomeScreen> {
           height: 150,
         ),
         actions: [
-          AppBarIconButton(onTap: () {  }, iconData:Icons.person,),
-          AppBarIconButton(onTap: () {  }, iconData:Icons.call,),
-          AppBarIconButton(onTap: () {  }, iconData:Icons.notification_add,)
-
-
+          AppBarIconButton(onTap: () {}, iconData: Icons.person),
+          AppBarIconButton(onTap: () {}, iconData: Icons.call),
+          AppBarIconButton(onTap: () {}, iconData: Icons.notification_add),
         ],
       ),
       body: Padding(
@@ -35,33 +38,72 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             const SizedBox(height: 16),
-            _build_search_bar()
+            _build_search_bar(),
+            const SizedBox(height: 16),
+            HomeBannerSlider(),
+            buildSelectionHeader(title: 'Categories', onTapSeeAll: () {
+              Get.find<MainNavController>().moveToCategory();
+
+            }),
+            const SizedBox(height: 16),
+
+            buildCategoryList(),
+            buildSelectionHeader(title: 'New', onTapSeeAll: () {}),
+            const SizedBox(height: 16),
+
+            buildSelectionHeader(title: 'Popular', onTapSeeAll: () {}),
           ],
         ),
       ),
     );
   }
 
+  SizedBox buildCategoryList() {
+    return SizedBox(
+      height: 100,
+      child: ListView.separated(
+
+        itemCount: 5,
+        primary: false,
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context,index) {
+          return ProductCategoryItem();
+        },
+        separatorBuilder: (context,index) {
+          return SizedBox(
+              width: 8);
+        },
+      ),
+    );
+  }
+
+  Widget buildSelectionHeader({
+    required String title,
+    required VoidCallback onTapSeeAll,
+  }) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(title, style: Theme.of(context).textTheme.titleMedium),
+        TextButton(onPressed: onTapSeeAll, child: Text("See all")),
+      ],
+    );
+  }
+
   Widget _build_search_bar() {
     return TextField(
-            onSubmitted: (String? text){
+      onSubmitted: (String? text) {},
+      textInputAction: TextInputAction.search,
+      decoration: InputDecoration(
+        hintText: 'Search',
 
-            },
-            textInputAction:TextInputAction.search,
-            decoration: InputDecoration(
-              hintText: 'Search',
-
-              fillColor: Colors.grey.shade300,
-              filled: true,
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide.none,
-
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide.none
-              ),
-              prefixIcon: Icon(Icons.search),
-            ),
-          );
+        fillColor: Colors.grey.shade300,
+        filled: true,
+        enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        prefixIcon: Icon(Icons.search),
+      ),
+    );
   }
 }
